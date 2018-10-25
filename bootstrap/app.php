2a7676c0621af6f683e3e7ecb2b0a8b117b8ca04
martin11106 <martin-adonis@hotmail.es>
2018-10-25 17:50:57 -0500

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,8 @@
 */
 
 $app = new Illuminate\Foundation\Application(
-    realpath(__DIR__.'/../')
+    realpath(require __DIR__.'/../vendor/autoload.php')
 );
-
 
 /*
 |--------------------------------------------------------------------------
